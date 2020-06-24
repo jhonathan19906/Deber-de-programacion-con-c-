32de71f2fea7c84b2fc224a5a4531c92bd801244
jhonathan19906 <47836888+jhonathan19906@users.noreply.github.com>
2020-06-24 16:15:31 -0500

    bien
    Este programa está creado con c#  es un pequeño formulario donde ingresamos datos y se imprimen por consola

---
+++ deber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NuevaAppDeProgramacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                Console.WriteLine(args[0]);
+            }
+            Console.WriteLine("BIENVENIDO INGRESA SUSDATOS");
+            Console.WriteLine("Nombre y Apellido:");
+            string datosleidos = Console.ReadLine();
+            Console.WriteLine("ESTADO CIVIL:");
+            string datosLeidos4 = Console.ReadLine();
+            Console.WriteLine("EDAD:");
+            string datosleidos1 = Console.ReadLine();
+            Console.WriteLine("TELEFONO:");
+            string datosleidos2 = Console.ReadLine();
+            Console.WriteLine("CUIDAD DONDE VIVE:");
+            string datosLeidos3 = Console.ReadLine();
+            Console.WriteLine("OCUPACION");
+            string datosLeidos5 = Console.ReadLine();
+            Console.WriteLine("HOLA  "+ datosleidos +" "+"Bienvenido al sistema "+" "+ datosLeidos4 +" "+"edad"+" "+ datosleidos1 +" "+"Telefono "+" "+ datosleidos2+
+            " "+"Cuidad de recidencia"+" "+ datosLeidos3+" "+"ocupacion"+" "+datosLeidos5);
+        }
+    }
+}
